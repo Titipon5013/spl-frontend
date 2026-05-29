11f32185e97f9dfcc3980cb8eb366626a1a3ff21
Titipon Tawong <titipon_tawong@cmu.ac.th>
2026-05-29 22:04:13 +0700

    update configuration to include tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
+import './App.css'
 
 ReactDom.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
